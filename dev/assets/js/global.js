97f13e7c5883adfefad9e49250b220c6b2c3dbcb
SergeyGvozdetskiy <gvozdetskiysergey@gmail.com>
2022-09-30 19:45:43 +0300

    new files

--- dev/assets/js/global.js
+++ dev/assets/js/global.js
@@ -229,16 +229,162 @@ jQuery(document).ready(function($) {
 		}
 	};
 
-	currentTime();
-	setInterval(currentTime, 1000);
-
 	const readMoreOpen = () => {
 		$('.js-more-open').on('click', function () {
 			$(this).closest('.our-happy__blockquote').children('.text-holder').toggleClass( "open");
 		})
 	}
 
+	const loginOpener = () => {
+		$('.js-login-opener').on('click', function () {
+			$('.js-login-opener').toggleClass('active');
+			$('.js-user-info-list').toggleClass('active');
+		});
+
+		$(document).mouseup(function (e) {
+			if ($('.js-user-info-list').has(e.target).length === 0){
+				$('.js-login-opener').removeClass('active');
+				$('.js-user-info-list').removeClass('active');
+			}
+		});
+	}
+
+	const datePicker = () => {
+		$( ".date-picker" ).datepicker({
+			changeMonth: true,
+			changeYear: true,
+		});
+	}
+
+	const mask = () => {
+		$(".phone").inputmask({"mask": "(999) 999-99-99"});
+	}
+
+	const customSelect = () => {
+		$( ".custom-select" ).selectmenu();
+	}
+
+	const addSocial = () => {
+		const addBtn = document.querySelector('.js-add-btn');
+		const addingBlocks = document.querySelector('.js-adding-socials');
+		const addBlock = `
+				<div class="socials-holder">
+					<div class="input-holder">
+						<strong class="input-label">Your social networks <i class="icomoon-privacy"><div class="privacy-text">Confidential field. Accessible only to the Matchmaker</div></i></strong>
+						<div class="social-block">
+							<select name="social" class="custom-select social-select">
+								<option selected="selected">Choose</option>
+								<option>Facebook</option>
+								<option>Twitter</option>
+								<option>Instagram</option>
+							</select>
+						</div>
+					</div>
+					<div class="input-holder">
+						<input type="text" placeholder="Your nickname/login ">
+					</div>
+				</div>`
+
+		if (addBtn) {
+			addBtn.addEventListener('click', () => {
+				addingBlocks.insertAdjacentHTML('beforeend', addBlock);
+				customSelect();
+			})
+		}
+	}
+
+	const addChildren = () => {
+		const addBtn = document.querySelector('.js-add-child-btn');
+		const addingBlocks = document.querySelector('.js-adding-children');
+		let count = 1;
+
+		if (addBtn) {
+			addBtn.addEventListener('click', () => {
+				console.log(count);
+				count += 1;
+				console.log(count);
+				const addBlock = `
+				<div class="status-block">
+\t\t\t\t\t\t\t\t\t\t<div class="input-holder">
+\t\t\t\t\t\t\t\t\t\t\t<label class="input-label" for="children-date-of-birth-${count}">Date of birth</label>
+\t\t\t\t\t\t\t\t\t\t\t<input id="children-date-of-birth-${count}" type="text" class="date-picker" placeholder="mm/dd/yyyy ">
+\t\t\t\t\t\t\t\t\t\t\t<i class="icomoon-calendar"></i>
+\t\t\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t\t\t<div class="input-holder input-holder_gender">
+\t\t\t\t\t\t\t\t\t\t\t<strong class="input-label">Gender</strong>
+\t\t\t\t\t\t\t\t\t\t\t<ul class="status-list">
+\t\t\t\t\t\t\t\t\t\t\t\t<li>
+\t\t\t\t\t\t\t\t\t\t\t\t\t<input type="radio" id="male-${count}" name="children-status-${count}" value="Male">
+\t\t\t\t\t\t\t\t\t\t\t\t\t<label for="male-${count}">Male</label>
+\t\t\t\t\t\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t\t\t\t\t\t<li>
+\t\t\t\t\t\t\t\t\t\t\t\t\t<input type="radio" id="female-${count}" name="children-status-${count}" value="Female">
+\t\t\t\t\t\t\t\t\t\t\t\t\t<label for="female-${count}">Female</label>
+\t\t\t\t\t\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t\t\t\t\t</ul>
+\t\t\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t\t\t</div>`
+				addingBlocks.insertAdjacentHTML('beforeend', addBlock);
+				datePicker();
+			})
+		}
+	}
+
+	const selectGifts = () => {
+		$( ".js-ladies-select" ).selectmenu({
+			change: function( event, ui ) {
+				$(this).closest('.select-holder').children('.btn').removeAttr("disabled");
+			}
+		});
+	}
+
+	const quantity = () => {
+
+		$('.plus').on('click', function () {
+			let input = $(this).closest('.quantity-holder').children('.quantity');
+			let price = $(this).closest('tr').children('td').children('.js-price');
+			let priceAll = $(this).closest('tr').children('td').children('.js-price-all');
+			let priceNumb = parseInt(price.text());
+			let priceAllNumb = parseInt(priceAll.text());
+			let inputValue = input.val();
+			let minValue =  parseInt(input.attr('min'));
+			let maxValue =  parseInt(input.attr('max'));
+
+			if (inputValue < maxValue) {
+				input.val(parseInt(inputValue) + 1);
+				console.log(1234);
+				priceAll.text((priceAllNumb + priceNumb));
+			}
+		});
+
+		$('.minus').on('click', function () {
+			let input = $(this).closest('.quantity-holder').children('.quantity');
+			let price = $(this).closest('tr').children('td').children('.js-price');
+			let priceAll = $(this).closest('tr').children('td').children('.js-price-all');
+			let priceNumb = parseInt(price.text());
+			let priceAllNumb = parseInt(priceAll.text());
+			let inputValue = input.val();
+			let minValue =  parseInt(input.attr('min'));
+			let maxValue =  parseInt(input.attr('max'));
+
+			if (inputValue >= minValue) {
+				input.val(parseInt(inputValue) - 1);
+				priceAll.text((priceAllNumb - priceNumb));
+			}
+		});
+	}
+
 	readMoreOpen();
+	currentTime();
+	setInterval(currentTime, 1000);
+	loginOpener();
+	datePicker();
+	mask();
+	customSelect();
+	addSocial();
+	addChildren();
+	selectGifts();
+	quantity();
 
 	AOS.init({
 		duration: 1000,
